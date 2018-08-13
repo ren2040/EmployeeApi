76a7dac4227454ef98e57219c7a8336831466d21
Renji Shaji <RShaji@ad.hackney.gov.uk>
2018-08-13 16:09:35 +0100

    first commit

---
+++ WebAPIDemo/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace WebAPIDemo.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Age { get; set; }
+    }
+}
